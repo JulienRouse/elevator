9891f1d72dd937fb2576cb53cd9a5f0ff9232607
JulienRouse <julien.rouse@gmail.com>
2015-07-24 16:10:35 +0200

    macro foireuse

--- src/elevator.lisp
+++ src/elevator.lisp
@@ -13,26 +13,34 @@
 (defparameter *simulation-speed* nil)
 
 ;;;;functions
+
+;;number of elevator 
 (defun set-elevator-number (n)
   (cond
-    ((not (typep n 'integer)) (print "error: elevator number must be an integer"))
-    ((< n 1) (print "error: elevator number must be a strictly positiv integer"))
+    ((not (typep n 'integer)) (princ "error: elevator number must be an integer"))
+    ((< n 1) (princ "error: elevator number must be a strictly positiv integer"))
     (t (setf *elevator-number* n))))
 
 ;;number of person fitting in the elevator
 (defun set-elevator-capacity-int (n)
   (cond
-    ((< n 1) (print "error: elevator capacity must be a strictly positiv integer"))
+    ((< n 1) (princ "error: elevator capacity must be a strictly positiv integer"))
     (t (setf *elevator-capacity* (list n)))))
 
 (defun set-elevator-capacity-list (l)
   (cond
     ((not (every #'(lambda (n) (> n 0)) l)) 
-     (print "error: elevator capacity must be a strictly positiv integer")) 
+     (princ "error: elevator capacity must be a strictly positiv integer")) 
     (t (setf *elevator-capacity* l))))
 
 (defun set-elevator-capacity (x)
   (cond
     ((typep x 'integer) (set-elevator-capacity-int x))
     ((typep x 'list) (set-elevator-capacity-list x))
-    (t (print "error: elevator capacity must be a integer, or a list of integer"))))
+    (t (princ "error: elevator capacity must be a integer, or a list of integer"))))
+
+;;
+(defun set-floor-number (n)
+  (cond
+    ((and (typep n 'integer) (> n 0)) (setf *floor-number* n))
+    (t (princ "error: elevator capacity must be a strictly positiv integer"))))
--- t/elevator.lisp
+++ t/elevator.lisp
@@ -2,13 +2,78 @@
 (defpackage elevator-test
   (:use :cl
         :elevator
-        :prove))
+	:prove))
 (in-package :elevator-test)
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :elevator)' in your Lisp.
+(defun mapa-b (fn a b &optional (step 1))
+  (do ((i a (+ i step))
+       (result nil))
+      ((> i b) (nreverse result))
+    (push (funcall fn i) result)))
 
-(plan nil)
+(defun map0-n (fn n)
+  (mapa-b fn 0 n))
 
-;; blah blah blah.
+
+(defmacro do-tuples/o (parms source &body body)
+  (if parms
+      (let ((src (gensym)))
+	`(prog ((,src ,source))
+	    (mapc #'(lambda ,parms ,@body)
+		  ,@(map0-n #'(lambda (n)
+				`(nthcdr  ,n ,src))
+			    (1- (length parms))))))))
+
+
+;;TODO ca ne marche pas du tout
+(defmacro create-subtest (function-name &rest rest)
+  (progn
+    (dolist (x rest)
+    `((first x) '(,function-name (,#'second ,x)) (third x)))))
+
+(defmacro tmp (fn )
+  `(subtest ,(concatenate 'string "Testing " (string fn))))
+
+
+(defmacro tmp2 (fn predicate param expected)
+  `(,predicate (,fn ,param) ,expected))
+
+(defmacro tmp3 (fn predicate param expected)
+  `(subtest ,(concatenate 'string "Testing " (string fn))
+     (,predicate (,fn ,param) ,expected)
+     (,predicate (,fn ,param) ,expected)))
+
+(defmacro tmp4 (fn &rest rest)
+  `(subtest ,(concatenate 'string "Testing " (string fn))
+    (dolist (x ,rest)
+      (let ((f (first x))
+	    (s (second x))
+	    (tt (third x)))
+	`(tmp2 fn ,f ,s ,tt))))))  
+    
+(defmacro tmp5 (fn &rest rest)
+  `(do-tuples/o (a b c) ,@rest
+     (funcall a (funcall ,fn b) c)))
+
+;(create-subtest (elevator:elevator (is 2 2) (is 3 3)
+;
+
+(defun run-test ()
+  (subtest "Testing set-elevator-number"
+    (is (elevator::set-elevator-number 2) 2)
+    (is-print (elevator::set-elevator-number "ok") "error: elevator number must be an integer")
+    (is-print (elevator::set-elevator-number -1) "error: elevator number must be a strictly positiv integer"))
+  (subtest "Testing set-elevator-capacity"
+    (is (elevator::set-elevator-capacity 2) '(2))
+    (is (elevator::set-elevator-capacity '(1 2 3)) '(1 2 3))
+    (is-print (elevator::set-elevator-capacity #(1 2 3)) "error: elevator capacity must be a integer, or a list of integer")
+    (is-print (elevator::set-elevator-capacity -2) "error: elevator capacity must be a strictly positiv integer")
+    (is-print (elevator::set-elevator-capacity '(1 2 -1)) "error: elevator capacity must be a strictly positiv integer")))
+  
+
+(plan 2)
+
+(run-test)
 
 (finalize)
